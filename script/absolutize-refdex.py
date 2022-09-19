b42b733cd05a9da6c5c2875ccebd4b9efc40bedd
Chris Pressey <cpressey@catseye.tc>
2022-09-19 14:42:18 +0100

    Absolute refdex links to point to GitHub.

--- script/absolutize-refdex.py
+++ script/absolutize-refdex.py
@@ -11,7 +11,7 @@ n = {}
 for key, value in data.items():
     assert 'anchor' in value and 'filename' in value
     n[key] = {
-        "url": "https://catseye.tc/view/The-Dossier/article/{}#{}".format(quote(value['filename']), value['anchor'])
+        "url": "https://github.com/cpressey/Some-Games-of-Note/blob/master/article/{}#{}".format(quote(value['filename']), value['anchor'])
     }
 
 sys.stdout.write(json.dumps(n, indent=4, sort_keys=True))
